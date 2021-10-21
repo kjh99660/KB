fb291de64fabbe32d8281d78b6a28ab6fd2afb94
kjh99660 <cghyuk@gmail.com>
2021-10-21 16:27:35 +0900

    10.21 backUp sikG

---
+++ KB/Assets/Scripts/Camera/CamaraManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraManager : MonoBehaviour
+{
+    public GameObject cameraFirstPos;
+    public GameObject cameraThirdPos;
+
+    private GameObject player;
+    private float SmoothTime = 0.1f, xmove = 0f, ymove = 0f;
+    private Vector3 cameraVelocity = Vector3.zero;
+    private Vector3 distancePlayerToCamera;
+    private Vector3 distancePlayerToCameraFirst;
+    private Transform ThirdPos;
+    private Transform FirstPos;
+    private Transform PlayerPos;
+
+    private void OnEnable()
+    {
+        player = GameObject.Find("MaleFree1");//Player
+        distancePlayerToCamera = new Vector3(0f, 8f, -5f);
+        distancePlayerToCameraFirst = new Vector3(0f, 0f, 1f);
+        ThirdPos = cameraThirdPos.transform;
+        FirstPos = cameraFirstPos.transform;
+        PlayerPos = player.transform;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(1))
+        {
+            xmove += Input.GetAxis("Mouse X");
+            ymove -= Input.GetAxis("Mouse Y");
+        }
+        
+        if(Input.GetKeyDown(KeyCode.F1))//시점 변경
+        {
+            if(cameraFirstPos.activeSelf)
+            {
+                cameraFirstPos.SetActive(false);
+                cameraThirdPos.SetActive(true);
+                xmove = 0; ymove = 0;
+            }
+            else
+            {
+                cameraFirstPos.SetActive(true);
+                cameraThirdPos.SetActive(false);
+                xmove = 0; ymove = 0;
+            }
+        }
+
+        ThirdPos.position = Vector3.SmoothDamp(ThirdPos.position, PlayerPos.position + distancePlayerToCamera, ref cameraVelocity, SmoothTime);
+        ThirdPos.rotation = Quaternion.Euler(35f + ymove, xmove, 0f);
+        FirstPos.position = Vector3.SmoothDamp(FirstPos.position, PlayerPos.position + distancePlayerToCameraFirst, ref cameraVelocity, SmoothTime);
+        FirstPos.rotation = Quaternion.Euler(-10f + ymove, xmove, 0f);
+    }
+}
